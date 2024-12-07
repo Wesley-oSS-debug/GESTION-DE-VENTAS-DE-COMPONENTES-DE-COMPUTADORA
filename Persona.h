9b1d67db4bf9ca6de4c80a9d838f535c661573fe
Oscar <oticonaf@unjbg.edu.pe>
2024-12-07 11:41:53 -0500

    Agrega clase Persona

---
+++ Persona.h
@@ -0,0 +1,19 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+#include <string>
+
+using namespace std;
+
+class Persona{
+	protected:
+		string nombre;
+		string dni;
+		string telefono;
+	public:
+		Persona(string,string,string);
+		void mostrarPersona();
+		string obtenerNombre();
+		string obtenerDNI();	
+};
+
+#endif
